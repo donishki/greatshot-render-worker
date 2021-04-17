2e9fb954de246ee27686f08bb8154c9d0de73669
Michal Mitter <mittermichal@gmail.com>
2021-04-18 00:48:58 +0200

    Revert "run_worker.bat: optional venv activation"
    This reverts commit c7c34e2d

--- run_worker.bat
+++ run_worker.bat
@@ -1,2 +1,1 @@
-cmd /k "cd /d venv\Scripts & activate & cd /d ..\.."
-cmd /k "dramatiq -t 1 -p 1 tasks render_heartbeat -Q render"
+cmd /k "cd /d venv\Scripts & activate & cd /d ..\.. & dramatiq -t 1 -p 1 tasks render_heartbeat -Q render"
